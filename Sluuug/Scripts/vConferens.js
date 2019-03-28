3aa677575da5de5605ee42419a08dc02b6465af5
Ilya Ryazanov <ilya.ryazanov@quipu.de>
2019-03-28 19:02:30 +0300

    remote streams are fills on caller and callee

--- Sluuug/Scripts/vConferens.js
+++ Sluuug/Scripts/vConferens.js
@@ -7,49 +7,14 @@ const peerConnCfg =
     'iceServers':
     [
         { url: 'stun:stun01.sipphone.com' },
-        { url: 'stun:stun.ekiga.net' },
-        { url: 'stun:stun.fwdnet.net' },
-        { url: 'stun:stun.ideasip.com' },
-        { url: 'stun:stun.iptel.org' },
-        { url: 'stun:stun.rixtelecom.se' },
-        { url: 'stun:stun.schlund.de' },
-        { url: 'stun:stun.l.google.com:19302' },
-        { url: 'stun:stun1.l.google.com:19302' },
-        { url: 'stun:stun2.l.google.com:19302' },
-        { url: 'stun:stun3.l.google.com:19302' },
-        { url: 'stun:stun4.l.google.com:19302' },
-        { url: 'stun:stunserver.org' },
-        { url: 'stun:stun.softjoys.com' },
-        { url: 'stun:stun.voiparound.com' },
-        { url: 'stun:stun.voipbuster.com' },
-        { url: 'stun:stun.voipstunt.com' },
-        { url: 'stun:stun.voxgratia.org' },
-        { url: 'stun:169.254.135.71:64609'},
-        { url: 'stun:stun.xten.com' },
-        {
-            url: 'turn:numb.viagenie.ca',
-            credential: 'muazkh',
-            username: 'webrtc@live.com'
-        },
-        {
-            url: 'turn:192.158.29.39:3478?transport=udp',
-            credential: 'JZEOEt2V3Qb0y27GRntt2u2PAYA=',
-            username: '28224511:1379330808'
-        },
-        {
-            url: 'turn:192.158.29.39:3478?transport=tcp',
-            credential: 'JZEOEt2V3Qb0y27GRntt2u2PAYA=',
-            username: '28224511:1379330808'
-        }
+        { url: 'stun:stun.ekiga.net' }
     ]
-    };
+};
 
-var localVideoElem, remoteVideoElem, localVideoStream, videoCallButton, endCallButton,
+var localVideo, remoteVideo, localVideoStream, videoCallButton, endCallButton,
     callerPeerConn, calleePeerConn = null;  
 
 window.addEventListener("load", checkWebrtc());
-console.log(videoChat);
-
 
 
 videoChat.on('GotInvite', function (callerName, callerSurName, offer, userId)
@@ -65,29 +30,18 @@ function initiate_call(friend_id)
 {
     var session_id = document.cookie.replace(/(?:(?:^|.*;\s*)session_id\s*\=\s*([^;]*).*$)|^.*$/, "$1");
     callerPeerConn = new RTCPeerConnection(peerConnCfg);
-    callerPeerConn.onaddstream = function (event) {
-        alert(1);
-    }
     callerPeerConn.ontrack = function (event) {
-        alert(21);
+        console.log('caller recived new stream');
+        remoteVideo.srcObject = event.streams[0];
     }
-
-    navigator.getUserMedia(
-        {
-            "audio": true, "video": { width: 200, height:200 }
-        },
-        function (stream)
-        {
+    navigator.mediaDevices.getUserMedia({ "audio": true, "video": true })
+        .then(function (stream) {
             localVideo.srcObject = stream;
-            callerPeerConn.addStream(stream);
-        },
-        function (err)
-        {
-            console.log(err.message);
-        }
-    );
-
-    callerPeerConn.createOffer()
+            for (const track of stream.getTracks()) {
+                callerPeerConn.addTrack(track, stream);
+            }
+            return callerPeerConn.createOffer();
+        })
         .then(
         function (offer) {
             var off = new RTCSessionDescription(offer);
@@ -101,105 +55,61 @@ function initiate_call(friend_id)
                             session: session_id,
                             to: friend_id
                         };
-
-                            videoChat.invoke('Invite', offer_dictionary);
-                            console.log('setLocalDescription');
-                            console.log(offer);
-                            console.log('send offer');
+                    videoChat.invoke('Invite', offer_dictionary);
+                    console.log('send offer');
                 },
                 function (err) {
-                    console.log("setLocalDescription fail");
                     console.log(err.message);
                 }
             )
         });
 }
 
-function incomming(callerName, callerSurName, offer, incommingUserId) {
-    var incommingOffer = new Object();
-    incommingOffer.type = offer.type;
-    incommingOffer.sdp = offer.sdp;
-    console.log("got offer");
-    console.log(offer);
-    var caller_phone = document.getElementById("called__" + incommingUserId);
-    if (caller_phone == null)
-    {
-        var mylist = $('#incomming');
-        mylist[0].insertAdjacentHTML('beforeend',
-            '<div class="incomming_call" id="called__' + incommingUserId + '">' +
-            '<img src= "https://res.cloudinary.com/dlk1sqmj4/image/upload/v1553527278/incomming_call.png" height= "45" width= "45"/>' +
-            '<span>' + callerName + '  ' + callerSurName + '</span> call to you ... </div>');
-        document.getElementById('called__' + incommingUserId).addEventListener(
-            'click', function () {
-                var caller_id = "called__" + incommingUserId;
-                accept_send_answer(incommingOffer, incommingUserId, caller_id);
-            })
-    }
- }
+
 
 function accept_send_answer(offer, userId, caller_id)
 {
-    try {
-        video = document.querySelector("#me_video");
-        navigator.mediaDevices.getUserMedia({ video: { width: 200, height: 200 }, audio: true })
-            .then(
-            function (stream) {
-                calleePeerConn = new RTCPeerConnection(peerConnCfg); // ??
-                calleePeerConn.onaddstream = function (event) {
-                    alert(1);
-                }
-                calleePeerConn.ontrack = function (event) {
-                    alert(21);
-                }
-                calleePeerConn.addStream(stream);
-            video.srcObject = stream;
-            console.log('----');
+    calleePeerConn = new RTCPeerConnection(peerConnCfg);
+    calleePeerConn.ontrack = function (event) {
+        console.log('callee accept offer and got streams');
+        remoteVideo.srcObject = event.streams[0];
+    }
+    calleePeerConn.setRemoteDescription(offer)
+        .then(function () {
+            return navigator.mediaDevices.getUserMedia({ "audio": true, "video": true });
+        })
+        .then(function (stream) {
+            localVideo.srcObject = stream; 
+            for (const track of stream.getTracks()) {
+                calleePeerConn.addTrack(track, stream);
+            }
+            return calleePeerConn.createAnswer();
+        })
+        .then(function (answer) {
+            var session_id = document.cookie.replace(/(?:(?:^|.*;\s*)session_id\s*\=\s*([^;]*).*$)|^.*$/, "$1");
+            var ansfer_dictionary =
+                {
+                    sdp: answer.sdp,
+                    type: answer.type,
+                    session: session_id,
+                    to: userId
+                };
 
+            videoChat.invoke('ConfirmInvite', ansfer_dictionary);
+            hide_incoming_if_accept(caller_id);
 
-            calleePeerConn.setRemoteDescription(
-                new RTCSessionDescription(offer),
-                function () {
-                    calleePeerConn.createAnswer(
-                        function (answer) {
-                            calleePeerConn.setLocalDescription(
-                            new RTCSessionDescription(answer),
-                            function () {
-                                var session_id = document.cookie.replace(/(?:(?:^|.*;\s*)session_id\s*\=\s*([^;]*).*$)|^.*$/, "$1");
-                                var ansfer_dictionary =
-                                    {
-                                        sdp: answer.sdp,
-                                        type: answer.type,
-                                        session: session_id,
-                                        to: userId
-                                    };
+            console.log("sending ansfer");
+            var remote_streams = calleePeerConn.getRemoteStreams();
+            var local_streams = calleePeerConn.getLocalStreams();
 
-                                videoChat.invoke('ConfirmInvite', ansfer_dictionary);
-                                console.log("sending ansfer");
-                                var remote_streams = calleePeerConn.getRemoteStreams();
-                                var local_streams = calleePeerConn.getLocalStreams();
-
-                                console.log("invited remote streams");
-                                console.log(remote_streams);
-                                console.log("invited local streams");
-                                console.log(local_streams);
-                                console.log(calleePeerConn);
-                                hide_incoming_if_accept(caller_id);
-                            },
-                            function (error) {
-                                console.log("setLocalDescription fail : " + error.message);
-                            });
-                    }, function (error) {
-                        console.log("create answer fail : " + error.message);
-                        });
-                },
-                function (error) {
-                    console.log("setRemoteDescription fail :"+error.message);
-                });
+            console.log("callee remote streams");
+            console.log(remote_streams);
+            console.log("callee local streams");
+            console.log(local_streams);
+        })
+        .catch(function (err) {
+            console.log(err.message);
         });
-    }
-    catch (error) {
-        console.log(error);
-    }
 }
 
 function got_ansfer(answer)
@@ -207,8 +117,6 @@ function got_ansfer(answer)
     callerPeerConn.setRemoteDescription(
         new RTCSessionDescription(answer),
         function () {
-            console.log("setRemoteDescription from ansfer");
-
             var remote_streams = callerPeerConn.getRemoteStreams();
             var local_streams = callerPeerConn.getLocalStreams();
 
@@ -216,13 +124,10 @@ function got_ansfer(answer)
             console.log(remote_streams);
             console.log("caller local streams");
             console.log(local_streams);
-            video = document.querySelector("#me_video");
-            navigator.mediaDevices.getUserMedia({ video : { width: 200, height: 200 }, audio: true })
+            navigator.mediaDevices.getUserMedia({ video: true, audio: true })
                 .then(
                 function (stream) {
-                    console.log("++");
-                    video.srcObject = stream;
-                    console.log(callerPeerConn);
+                    localVideo.srcObject = stream;
                 },
                 function (err) {
                     console.log("set local video caller error");
@@ -234,9 +139,6 @@ function got_ansfer(answer)
         }
     );
 }
-
-
-
 
 function checkWebrtc() {
     if (navigator.getUserMedia) {
@@ -252,6 +154,27 @@ function checkWebrtc() {
     }
     else {
         alert("Sorry, your browser does not support WebRTC!");
+    }
+}
+
+function incomming(callerName, callerSurName, offer, incommingUserId) {
+    var incommingOffer = new Object();
+    incommingOffer.type = offer.type;
+    incommingOffer.sdp = offer.sdp;
+    console.log("got offer");
+    console.log(offer);
+    var caller_phone = document.getElementById("called__" + incommingUserId);
+    if (caller_phone == null) {
+        var mylist = $('#incomming');
+        mylist[0].insertAdjacentHTML('beforeend',
+            '<div class="incomming_call" id="called__' + incommingUserId + '">' +
+            '<img src= "https://res.cloudinary.com/dlk1sqmj4/image/upload/v1553527278/incomming_call.png" height= "45" width= "45"/>' +
+            '<span>' + callerName + '  ' + callerSurName + '</span> call to you ... </div>');
+        document.getElementById('called__' + incommingUserId).addEventListener(
+            'click', function () {
+                var caller_id = "called__" + incommingUserId;
+                accept_send_answer(incommingOffer, incommingUserId, caller_id);
+            })
     }
 }
 
