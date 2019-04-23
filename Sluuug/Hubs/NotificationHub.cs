b1ac5f577e5098b0387e27b00ade13b3573b09af
Ilya Ryazanov <ilya.ryazanov@quipu.de>
2019-04-23 18:39:49 +0300

    userConnectIDs tbo fill complete

---
+++ Sluuug/Hubs/NotificationHub.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using System.Web;
+using Microsoft.AspNet.SignalR;
+using Slug.Helpers;
+
+namespace Slug.Hubs
+{
+    public class NotificationHub : Hub
+    {
+
+        public void OpenConnect()
+        {
+            var UCW = new UserConnectionWorker();
+            string session = base.Context.Request.Cookies["session_id"].Value;
+            string connection = Context.ConnectionId;
+            UCW.AddConnection(connection, session);
+        }
+
+        public void CloseConnect()
+        {
+            var UCW = new UserConnectionWorker();
+            string session = base.Context.Request.Cookies["session_id"].Value;
+            string connection = Context.ConnectionId;
+            UCW.CloseConnection(connection, session);
+        }
+
+        public async Task NewChatMessage()
+        {
+            //Clients.Clients().GotNewChatMessage();
+        }
+
+        public async Task NewCryptoChatMessage()
+        {
+            //Clients.Others.GotNewCryptoChatMessage();
+        }
+
+        public async Task NewVideoConverence()
+        {
+            //Clients.Others.NewVideoConverence();
+        }
+    }
+}
--- Sluuug/Scripts/notificationHub.js
+++ Sluuug/Scripts/notificationHub.js
@@ -1,1 +1,14 @@
-﻿
+﻿var connection = $.hubConnection();
+var hub = connection.createHubProxy('notificationHub');
+
+window.onload = function () {
+    connection.start().done(
+        function () {
+            hub.invoke('OpenConnect')
+        });
+}
+
+
+window.onunload = function () {
+    hub.invoke('CloseConnect');
+}
