5acb33f199d69f34e942f029c17e92faffc4f0cf
Ilya Ryazanov <ilya.ryazanov@quipu.de>
2020-02-10 18:51:22 +0300

    YandexPayHandler

---
+++ Sluuug/Helpers/Handlers/PrivateUserServices/Payments/YandexPayHandler.cs
@@ -0,0 +1,37 @@
+﻿using Context;
+using SharedModels.Yandex;
+using Slug.Context.Tables;
+using System;
+using System.Linq;
+
+namespace Slug.Helpers.Handlers.PrivateUserServices.Payments
+{
+    public class YandexPayHandler
+    {
+        public void RegisterTransaction(StartTransaction request, string sessionNumber)
+        {
+            using (var context = new DataBaseContext())
+            {
+                Session session = context.Sessions.First(x => x.Number == sessionNumber);
+                User user = context.Users.First(x => x.Id == session.UserId);
+
+                Transaction transaction = new Transaction();
+                transaction.Session = sessionNumber;
+                transaction.PaySender = user;
+                transaction.Type = request.Type;
+                transaction.InternalId = request.TransactionId;
+                transaction.Amount = request.Amount;
+                transaction.CreateDate = DateTime.UtcNow;
+
+                context.Transactions.Add(transaction);
+                context.SaveChanges();
+            }
+        }
+
+        public void CompleteTransaction()
+        {
+
+
+        }
+    }
+}
